dd17b118964ad10320af854c09e6dcf14d9348a5
BrunoSoffo <brussoffo@gmail.com>
2023-08-14 00:47:56 -0300

    Instânciando classe (criando obj.)

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pessoa titular1 = new Pessoa();
+        titular1.nome = "Bruno Soffo";
+        titular1.documento = "4444444";
+
+        Pessoa titular2 = new Pessoa();
+        titular2.nome = "Maria Lina";
+        titular2.documento = "5555555";
+
+        Conta minhaConta = new Conta();
+        minhaConta.titular = titular1;
+        minhaConta.agencia = 123;
+        minhaConta.numero = 999;
+        minhaConta.saldo = 15_000;
+
+        Conta suaConta = new Conta();
+        suaConta.titular = titular2;
+        suaConta.agencia = 222;
+        suaConta.numero = 111;
+        suaConta.saldo = 25_000;
+
+        System.out.println("Titular: " + minhaConta.titular.nome);
+        System.out.println("Saldo: " + minhaConta.saldo);
+
+        System.out.println("Titular: " + suaConta.titular.nome);
+        System.out.println("Saldo: " + suaConta.saldo);
+    }
+}
